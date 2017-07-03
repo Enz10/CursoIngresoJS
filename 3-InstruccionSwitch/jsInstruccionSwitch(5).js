acf2725abbd432d5a0c4d5981cb06df4b25e4fab
Enz10 <dinoquito10>
2017-07-03 11:17:07 -0300

    Ejercicios

--- 3-InstruccionSwitch/jsInstruccionSwitch(5).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(5).js
@@ -7,29 +7,13 @@ var Hora = document.getElementById('hora').value;
 switch(Hora)
 {
     case "7":
-    {
-        alert("Estas entre las 7 y 11. Es de mañana.");
-        break;
-    }
 
     case "8":
-    {
-        alert("Estas entre las 7 y 11. Es de mañana.");
-        break;
-    }
 
     case "9":
-    {
-        alert("Estas entre las 7 y 11. Es de mañana.");
-        break;
-    }
 
     case "10":
-    {
-        alert("Estas entre las 7 y 11. Es de mañana.");
-        break;
-    }
-
+    
     case "11":
     {
         alert("Estas entre las 7 y 11. Es de mañana.");
--- 3-InstruccionSwitch/jsInstruccionSwitch(6).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(6).js
@@ -6,34 +6,14 @@ var Hora = document.getElementById('hora').value;
 switch(Hora)
 {
     case "1":
-    {
-        alert("Es de noche.");
-        break;
-    }
 
     case "2":
-    {
-        alert("Es de noche.");
-        break;
-    }
 
     case "3":
-    {
-        alert("Es de noche.");
-        break;
-    }
 
     case "4":
-    {
-        alert("Es de noche.");
-        break;
-    }
 
     case "5":
-    {
-        alert("Es de noche.");
-        break;
-    }
 
     case "6":
     {
@@ -42,28 +22,12 @@ switch(Hora)
     }
 
     case "7":
-    {
-        alert("Es de mañana.");
-        break;
-    }
 
     case "8":
-    {
-        alert("Es de mañana.");
-        break;
-    }
 
     case "9":
-    {
-        alert("Es de mañana.");
-        break;
-    }
 
     case "10":
-    {
-        alert("Es de mañana.");
-        break;
-    }
 
     case "11":
     {
@@ -72,46 +36,18 @@ switch(Hora)
     }
 
     case "12":
-    {
-        alert("Es de tarde.");
-        break;
-    }
 
     case "13":
-    {
-        alert("Es de tarde.");
-        break;
-    }
 
     case "14":
-    {
-        alert("Es de tarde.");
-        break;
-    }
 
     case "15":
-    {
-        alert("Es de tarde.");
-        break;
-    }
 
     case "16":
-    {
-        alert("Es de tarde.");
-        break;
-    }
 
     case "17":
-    {
-        alert("Es de tarde.");
-        break;
-    }
 
     case "18":
-    {
-        alert("Es de tarde.");
-        break;
-    }
 
     case "19":
     {
@@ -120,28 +56,12 @@ switch(Hora)
     }
 
     case "20":
-    {
-        alert("Es de noche.");
-        break;
-    }
 
     case "21":
-    {
-        alert("Es de noche.");
-        break;
-    }
 
     case "22":
-    {
-        alert("Es de noche.");
-        break;
-    }
 
     case "23":
-    {
-        alert("Es de noche.");
-        break;
-    }
 
     case "24":
     {
