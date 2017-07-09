8c102bf1258b443d4cb40e330757dccbc7290535
Enz10 <enzobal6@gmail.com>
2017-07-09 20:15:31 -0300

    Ejercicios

--- 5-InstruccionWhile/jsIteraciones (8).js
+++ 5-InstruccionWhile/jsIteraciones (8).js
@@ -2,10 +2,30 @@ function Mostrar()
 {
 
 	var contador=0;
+	var acumulador=0;
 	var positivo=0;
 	var negativo=1;
+	var respuesta=true;
+
 	
-	var respuesta='si';
+
+	while(respuesta==true)
+	{
+		contador++;
+		acumulador=prompt("Ingrese un número.");
+		positivo=
+        positivo=parseInt(positivo);
+
+
+
+
+
+
+
+
+	   respuesta=confirm("¿Desea seguir ingresando números?")
+
+	}
 
 
 document.getElementById('suma').value=positivo;
